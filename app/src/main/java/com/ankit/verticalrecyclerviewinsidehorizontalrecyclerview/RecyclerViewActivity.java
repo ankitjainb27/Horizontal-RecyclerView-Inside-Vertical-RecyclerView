fe8b38e41eb2b3db10e31d3e08b46516b38b42c9
ankitjainb27 <ankit.jainb27@gmail.com>
2015-09-11 16:05:05 +0530

    initial commit

---
+++ app/src/main/java/com/ankit/verticalrecyclerviewinsidehorizontalrecyclerview/RecyclerViewActivity.java
@@ -0,0 +1,42 @@
+package com.ankit.verticalrecyclerviewinsidehorizontalrecyclerview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends Activity {
+
+    private List<Person> persons;
+    private RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recyclerview_activity);
+        rv = (RecyclerView) findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+        rv.setLayoutManager(llm);
+        rv.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+    }
+
+    private void initializeData() {
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    private void initializeAdapter() {
+        RVAdapter adapter = new RVAdapter(persons);
+        rv.setAdapter(adapter);
+    }
+}
